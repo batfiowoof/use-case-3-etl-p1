efa24cfc40d1d6c0cecbee2c87ecbf6edfc4fd0e
batfiowoof <bojidarlambov@abv.bg>
2025-03-25 13:08:35 +0200

    added all tables, updates and deletes, dupe extract left todo

--- sql/ecommerce_db.sql
+++ sql/ecommerce_db.sql
@@ -5,6 +5,8 @@ USE DATABASE ECOMMERCE_DB;
 CREATE SCHEMA IF NOT EXISTS STAGE_EXTERNAL;
 CREATE SCHEMA IF NOT EXISTS TABLE_S3;
 
+CREATE SCHEMA IF NOT EXISTS CORRECTIONS;
+
 USE SCHEMA STAGE_EXTERNAL;
 
 CREATE STAGE IF NOT EXISTS STAGE_EXTERNAL.STAGE_ECOMMERCE -- https://docs.snowflake.com/en/sql-reference/sql/create-stage
@@ -17,7 +19,7 @@ CREATE OR REPLACE TABLE TABLE_S3.TD_S3(
     order_id int,
     customer_id string,
     customer_name string,
-    order_date date,
+    order_date string,
     product string,
     quantity int,
     price float,
@@ -34,4 +36,70 @@ FILE_FORMAT = (TYPE = 'CSV'
     SKIP_HEADER = 1
     FIELD_OPTIONALLY_ENCLOSED_BY = '"' -- защото snowflake гърми и мисли че има 13 колони :^)
     )
-ON_ERROR = 'CONTINUE';
+ON_ERROR = 'CONTINUE'
+;
+
+
+USE SCHEMA CORRECTIONS;
+
+-- Ако Адреса за доставка липсва, но статуса е Delivered - прехвърлете записа към отделна таблица, която да съдържа само и единствено такива доставки, гонови за ревю, td_for_review
+CREATE OR REPLACE TABLE CORRECTIONS.TD_FOR_REVIEW LIKE TABLE_S3.TD_S3;
+
+INSERT INTO CORRECTIONS.TD_FOR_REVIEW
+SELECT * FROM TABLE_S3.TD_S3
+WHERE SHIPPING_ADDRESS IS NULL AND STATUS = 'Delivered';
+
+-- Ако в записа липсва данни за клиента Customer_id , то тогава този запис трябва да бъде прехвърлен към таблица td_suspisios_records
+CREATE OR REPLACE TABLE CORRECTIONS.TD_SUSPICIOUS_RECORDS LIKE TABLE_S3.TD_S3;
+
+INSERT INTO CORRECTIONS.TD_SUSPICIOUS_RECORDS
+SELECT * FROM TABLE_S3.TD_S3
+WHERE CUSTOMER_NAME IS NULL;
+
+-- Ако липсва информация за платежния метод, коригирайте със стойност по подразбиране Unknown
+USE SCHEMA TABLE_S3;
+UPDATE TABLE_S3.TD_S3
+SET PAYMENT_METHOD = 'UNKNOWN'
+WHERE PAYMENT_METHOD IS NULL;
+
+USE SCHEMA CORRECTIONS;
+
+-- Невалиден формат на дата
+CREATE OR REPLACE TABLE CORRECTIONS.TD_INVALID_DATE_FORMAT LIKE TABLE_S3.TD_S3;
+
+INSERT INTO CORRECTIONS.TD_INVALID_DATE_FORMAT
+SELECT * FROM TABLE_S3.TD_S3
+WHERE TRY_TO_DATE(ORDER_DATE) is  null; -- https://docs.snowflake.com/en/sql-reference/functions/try_to_date
+
+-- Отрицателни или нулеви стойности за количество и цена
+CREATE OR REPLACE TABLE CORRECTIONS.TD_INVALID_COUNT_AND_PRICE LIKE TABLE_S3.TD_S3;
+
+INSERT INTO CORRECTIONS.TD_INVALID_COUNT_AND_PRICE
+SELECT * FROM TABLE_S3.TD_S3
+WHERE QUANTITY < 0 OR PRICE < 0;
+
+-- Невалидна отстъпка
+UPDATE TABLE_S3.TD_S3
+SET DISCOUNT = 0
+WHERE DISCOUNT < 0;
+
+UPDATE TABLE_S3.TD_S3
+SET DISCOUNT = 0.50
+WHERE DISCOUNT > 0.50;
+
+-- Неправилно калкулирана крайна цена (където се налага)
+UPDATE TABLE_S3.TD_S3
+SET TOTAL_AMOUNT = QUANTITY * PRICE * (1 - DISCOUNT)
+WHERE TOTAL_AMOUNT != QUANTITY * PRICE * (1 - DISCOUNT);
+
+-- Неконсистентни данни за статуса на поръчката
+UPDATE TABLE_S3.TD_S3
+SET STATUS = 'Pending'
+WHERE SHIPPING_ADDRESS IS NULL;
+
+-- Изтриване на повтарящи се записи
+CREATE OR REPLACE TABLE CORRECTIONS.TD_DUPLICATES LIKE TABLE_S3.TD_S3;
+
+INSERT INTO CORRECTIONS.TD_DUPLICATES
+SELECT DISTINCT ORDER_ID FROM TABLE_S3.TD_S3;
+
